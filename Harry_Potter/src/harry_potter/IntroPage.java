2f595e107926b68abb3cae6ac16e231abc5818fb
Sai Anurag <anuragsai97@gmail.com>
2016-11-18 09:44:11 +0530

    game_intro_video

---
+++ Harry_Potter/src/harry_potter/IntroPage.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harry_potter;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+/**
+*
+* @author erix7
+*/
+public class IntroPage extends Application {
+ 
+ /**
+  * @param args the command line arguments
+  */
+ public static void main(String[] args) {
+     launch(args);
+ }
+ 
+ @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        root.setId("pane");
+        Scene scene = new Scene(root, 800, 600);
+        //scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        //Canvas canvas = new Canvas( 1366, 768 );
+        //root.getChildren().add( canvas );
+
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ Harry_Potter/src/harry_potter/IntroVideo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harry_potter;
+
+import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+ 
+/**
+*
+* @author erix7
+*/
+public class IntroVideo extends Application {
+ 
+ /**
+  * @param args the command line arguments
+  */
+ public static void main(String[] args) {
+     launch(args);
+ }
+ 
+ @Override
+ public void start(Stage theStage) {
+     
+    Media media = new Media(new File("C:/Users/PDN SRNIVAS/Documents/NetBeansProjects/Harry_Potter/src/harry_potter/Intro Video.mp4").toURI().toString());
+
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.setAutoPlay(true);
+    MediaView mediaView = new MediaView(mediaPlayer);
+    //BorderPane borderPane = new BorderPane();
+    //borderPane.setCenter(mediaView);
+    //borderPane.setPrefSize(1366, 700);
+    //borderPane.setCenter(mediaView);
+    //borderPane.setBottom(addToolBar());
+     
+    StackPane root = new StackPane();
+    root.getChildren().add(mediaView);
+    mediaView.setFitWidth(1366);
+    mediaView.setFitHeight(768);
+    theStage.setTitle("Harry Potter");
+    //mediaView.setStyle(STYLESHEET_MODENA);
+   
+    //borderPane.setStyle("-fx-background-color: Black" 1366 700);
+    Scene scene = new Scene(root, 800, 600);
+    
+    scene.setFill(Color.rgb(0, 0, 0));
+     //Make scene occupy full screen
+    //Scene scene = new Scene(root, 1366, 700); 
+    //Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
+   
+     theStage.setScene(scene);
+     theStage.show();
+ }
+}
