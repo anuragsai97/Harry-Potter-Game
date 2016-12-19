a018706d9735f90c3c25c55e73c22f29fa2a530b
Sai Anurag <anuragsai97@gmail.com>
2016-12-19 11:42:07 +0530

    final update

--- Harry_Potter/src/harry_potter/Test.java
+++ Harry_Potter/src/harry_potter/Test.java
@@ -108,6 +108,8 @@ public class Test extends Application implements Runnable{
                 }
               //  */
                 
+               //kks_t.add("reducto");
+               //kks_t.add("Wingardium Leviosa");
                 
         
 
